75ddd10640d70d21ce5e84261ee8e6b2b1cbe73f
Alexa Hendry <alexahendry88@gmail.com>
2023-03-20 17:22:27 +0000

    Add css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+/* create an object 'styles' with all the css from the file */
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+    return (
+        /* styles is the imported object above, and .App is the class in the css file */
+        <div className={styles.App}>
+            <NavBar />
+        </div>
+    );
 }
 
 export default App;
