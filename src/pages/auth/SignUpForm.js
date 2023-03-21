1926533bee401fc7c1c7304041c539ad5520bf68
Alexa Hendry <alexahendry88@gmail.com>
2023-03-21 13:05:42 +0000

    Complete sign up form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,28 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+    });
+    const { username, password1, password2 } = signUpData;
+
+    /* store history with react router */
+    const history = useHistory();
+
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (event) => {
+        setSignUpData({
+            /* spread previous signUpData so only the relevant attribute is updated */
+            ...signUpData,
+            /* creates key: value pair of name: value */
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        /* prevents the page from refreshing */
+        event.preventDefault();
+        try {
+            /* redirect to url and include the signUpData entered */
+            await axios.post("dj-rest-auth/registration/", signUpData);
+            history.push("/signin");
+        } catch (err) {
+            /* ? is optional chaining. If there is no response, no error is thrown */
+            setErrors(err.response?.data)
+        }
+    };
+
     return (
         <Row className={styles.Row}>
             <Col className="my-auto py-2 p-md-2" md={6}>
                 <Container className={`${appStyles.Content} p-4 `}>
                     <h1 className={styles.Header}>sign up</h1>
 
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="username">
                             {/* className="d-none" removes the label for screen readers */}
-                            <Form.Label className="d-none">username</Form.Label>
+                            <Form.Label className="d-none">Username</Form.Label>
                             <Form.Control
                                 className={styles.Input}
                                 type="text"
-                                placeholder="username"
+                                placeholder="Username"
                                 name="username"
+                                value={username}
+                                onChange={handleChange}
                             />
                         </Form.Group>
+                        {/* display error if username is present */}
+                        {errors.username?.map((message, idx) => 
+                            <Alert variant="warning" key={idx}>{message}</Alert>
+                        )}
 
                         <Form.Group controlId="password1">
                             <Form.Label className="d-none">Password</Form.Label>
@@ -31,26 +73,44 @@ const SignUpForm = () => {
                                 type="password"
                                 placeholder="Password"
                                 name="password1"
+                                value={password1}
+                                onChange={handleChange}
                             />
                         </Form.Group>
+                        {errors.password1?.map((message, idx) => 
+                            <Alert variant="warning" key={idx}>{message}</Alert>
+                        )}
 
                         <Form.Group controlId="password2">
-                            <Form.Label className="d-none">Confirm password</Form.Label>
+                            <Form.Label className="d-none">
+                                Confirm password
+                            </Form.Label>
                             <Form.Control
                                 className={styles.Input}
                                 type="password"
                                 placeholder="Confirm password"
                                 name="password2"
+                                value={password2}
+                                onChange={handleChange}
                             />
                         </Form.Group>
+                        {errors.password2?.map((message, idx) => 
+                            <Alert variant="warning" key={idx}>{message}</Alert>
+                        )}
 
-                        <Button 
+                        <Button
                             className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-                            type="submit">
+                            type="submit"
+                        >
                             Sign up
                         </Button>
-                    </Form>
+                        {/* non_field_error for buttons etc */}
+                        {errors.non_field_errors?.map((message, idx) =>
+                            /* className mt-3 adds margin */
+                            <Alert variant="warning" key={idx} className="mt-3">{message}</Alert>
+                        )}
 
+                    </Form>
                 </Container>
                 <Container className={`mt-3 ${appStyles.Content}`}>
                     <Link className={styles.Link} to="/signin">
