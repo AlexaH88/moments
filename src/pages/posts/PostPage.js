6a4d53765a72e0f1e39e7aedba0601aba8e4161c
Alexa Hendry <alexahendry88@gmail.com>
2023-03-25 14:19:41 +0000

    Create post component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
     /* fetch post by id */
@@ -37,7 +38,10 @@ function PostPage() {
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles for mobile</p>
-                <p>Post component</p>
+                {/* spread post results so that its key value pairs are passed in as props */}
+                {/* include setPosts function and setPost prop which will be used for likes */}
+                {/* inclue postPage prop so that we can have the user edit and delete their own posts */}
+                <Post {...post.results[0]} setPosts={setPost} postPage />
                 <Container className={appStyles.Content}>Comments</Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
