48019aff15b08314e3c8b93eea4953c05346717d
Alexa Hendry <alexahendry88@gmail.com>
2023-03-27 16:46:16 +0000

    Set up popular profiles component

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -12,6 +12,7 @@ import NoResults from "../../assets/no-results.png";
 import Asset from "../../components/Asset";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 /* add message and filter props, filter as an empty string initially */
 function PostsPage({ message, filter = "" }) {
@@ -112,7 +113,7 @@ function PostsPage({ message, filter = "" }) {
                 )}
             </Col>
             <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-                <p>Popular profiles for desktop</p>
+                <PopularProfiles />
             </Col>
         </Row>
     );
